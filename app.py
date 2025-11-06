7840d21baa0c04c5cbb9beee3e425e012debb925
kuma <kuma@babylon-tech.com>
2025-11-06 10:52:30 +0900

    Chat Completions APIの呼び出し処理を追加

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+import os
+
 import streamlit as st
+from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
+
+load_dotenv()
 
 st.title("langchain-streamlit-app")
 
@@ -18,7 +25,12 @@ if prompt:
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        response = "こんにちは"
-        st.markdown(response)
+        chat = ChatOpenAI(
+            model_name=os.environ["OPENAI_API_MODEL"],
+            temperature=os.environ["OPENAI_API_TEMPERATURE"],
+        )
+        messages = [HumanMessage(content=prompt)]
+        response = chat(messages)
+        st.markdown(response.content)
 
     st.session_state.messages.append({"role": "assistant", "content": response})
